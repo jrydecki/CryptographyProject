ba0805e592dbd61d1fe4331fe5731fc3df654816
Ezra Lovett <ezra.lovett@gmail.com>
2024-11-05 16:54:05 -0700

    Changes I made. If this is dumb let me know!

--- outline.cpp
+++ outline.cpp
@@ -1,38 +1,91 @@
 
 #include <iostream>
-#include <openssl/evp.h>
+#include <openssl/aes.h>
+#include <openssl/des.h>
 #include <openssl/rand.h>
 #include <cstring>
-#include <chrono>
 
 using namespace std;
-using namespace std::chrono;
 
-void GenerateKey(int byte_length){
-
+void GenerateAESKey(unsigned char* byte_length){
+	// generate key for AES Modes
 } // end GenerateKey
 
-void GenerateIV(int byte_length){
-
+void GenerateAESIV(unsigned char* byte_length){
+	// generate IV for AES Modes
 } // end GenerateIV
 
-void AES_Encrypt(char* iv, char* key, char* plaintext, int mode){
+void GenerateDESKey(unsigned char* byte_length){
+	// generate key for DES Modes
+} // end GenerateKey
+
+void GenerateDESIV(unsigned char* byte_length){
+	// generate IV for DES Modes
+} // end GenerateIV
+
+void AES_Encrypt(unsigned char* iv, unsigned char* key, unsigned char* plaintext, int mode){
+AES_KEY encryptKey;
+AES_set_encrypt_key(key, 256, &encryptKey);
+
+	// logic for encryption in each mode, using
+	AES_encrypt();
+	// for encrypting single block
 
 } // end AES_Encrypt
 
-void AES_Decrypt(char* iv, char* key, char* ciphertext, int mode){
+void AES_Decrypt(unsigned char* iv, unsigned char* key, unsigned char* ciphertext, int mode){
+AES_KEY decryptKey;
+AES_set_decrypt_key(key, 256, &decryptKey);
 
-} // end DES_Decrypt
+	// logic for encryption in each mode, using
+	AES_decrypt();
+	// for encrypting single block
+
+} // end AES_Decrypt
+
+void DES_CBC_Encrypt(unsigned char* plaintext, unsigned char* ciphertext, int length, DES_cblock& key, DES_cblock& iv) {
+    DES_key_schedule schedule;
+    DES_set_key_unchecked(&key, &schedule);
+
+	// logic for encryption in each mode, using
+	DES_encrypt1();
+	// for encrypting single block
+
+} // end of DES_Encrypt
+
+void DES_CBC_Decrypt(unsigned char* ciphertext, unsigned char* plaintext, int length, DES_cblock& key, DES_cblock& iv) {
+    DES_key_schedule schedule;
+    DES_set_key_unchecked(&key, &schedule);
+
+	// logic for encryption in each mode, using
+	DES_encrypt1();
+	// which is also used for decrypting single block
+
+} // end of DES_Decrypt
+
+// CTR and OFB modes for both AES and DES would similarly involve 
+// setting up counter increments for CTR or feedback chaining for OFB, 
+// but with the adjusted block size for DES (64-bit blocks).
 
 
 
 
 int main() {
+	// Test data
+    unsigned char plaintext[64] = "Example plaintext for AES and DES in CBC, CTR, and OFB modes.";
+    unsigned char ciphertext[64];
+    unsigned char decryptedtext[64];
 
-	// How to Time
-	auto start = high_resolution_clock::now();
-	auto stop = high_resolution_clock::now();
-	auto duration = duration_cast<microseconds>(stop - start);
-	cout << duration.count() << endl;
+    // AES Key and IV
+    unsigned char aes_key[32];  // AES-256 key (32 bytes)
+    unsigned char aes_iv[16];   // AES IV (16 bytes)
+    GenerateAESKey(aes_key);
+    GenerateAESIV(aes_iv);
+
+    // DES Key and IV
+    DES_cblock des_key;
+    DES_cblock des_iv;
+    GenerateDESKey(des_key);
+    GenerateDESIV(des_iv);
 
 } // end main
